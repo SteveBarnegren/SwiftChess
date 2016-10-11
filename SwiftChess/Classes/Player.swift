c101cc313b18e4476306d8a760c85cae102e48cf
Steve Barnegren <steve.barnegren@gmail.com>
2016-10-11 22:33:23 +0100

    Implement player ability to move pieces

--- SwiftChess/Classes/Player.swift
+++ SwiftChess/Classes/Player.swift
@@ -18,35 +18,74 @@ open class Player {
         self.game = game;
     }
     
-    func movePiece(fromLocation: BoardLocation, toLocation: BoardLocation){
+    // MARK: - Public
+    
+    public func movePiece(fromLocation: BoardLocation, toLocation: BoardLocation) {
+        
+        // Check if move is allowed
+        let canMove = canMovePieceWithError(fromLocation: fromLocation, toLocation: toLocation)
+        
+        if let error = canMove.error {
+            error.printDescription()
+            print("To avoid seeing these errors, call canMovePiece(fromLocation:toLocation) first")
+            return
+        }
+        
+        // Move the piece
+        game.board.movePiece(fromLocation: fromLocation, toLocation: toLocation)
+    }
+    
+    public func canMovePiece(fromLocation: BoardLocation, toLocation: BoardLocation) -> Bool {
+        return canMovePieceWithError(fromLocation: fromLocation, toLocation: toLocation).result
+    }
+
+    // MARK: - Private
+    
+    enum PieceMoveError : Error {
+        case movingToSameLocation(description: String)
+        case noPieceToMove(description: String)
+        case pieceColorDoesNotMatchPlayerColor(description: String)
+        case pieceUnableToMoveToLocation(description: String)
+        
+        func printDescription() {
+            
+            switch self {
+            case .movingToSameLocation(let description):
+                print(description)
+            case .noPieceToMove(let description):
+                print(description)
+            case .pieceColorDoesNotMatchPlayerColor(let description):
+                print(description)
+            case .pieceUnableToMoveToLocation(let description):
+                print(description)
+            }
+        }
+    }
+    
+    private func canMovePieceWithError(fromLocation: BoardLocation, toLocation: BoardLocation) -> (result: Bool, error: PieceMoveError?) {
         
         // We can't move to our current location
         if fromLocation == toLocation {
-            print("Cannot move piece to its current location")
+            return (false, .movingToSameLocation(description: "Cannot move piece to its current location"))
         }
         
         // Get the piece
         guard let piece = self.game.board.getPiece(at: fromLocation) else {
-            print("There isn't a piece of color \(color) at \(fromLocation)")
-            return
+            return (false, .noPieceToMove(description: "There isn't a piece of color \(color) at \(fromLocation)"))
         }
         
         // Check that the piece color matches the player color
         if piece.color != self.color {
-            print("Player color \(color) cannot move piece of color \(piece.color)")
-            return;
+            return (false, .pieceColorDoesNotMatchPlayerColor(description: "Player color \(color) cannot move piece of color \(piece.color)"))
         }
         
         // Make sure the piece can move to the location
         if !piece.movement.canPieceMove(fromLocation: fromLocation, toLocation: toLocation, board: game.board) {
-            print("Piece at \(fromLocation) cannot move to \(toLocation)")
+            return (false, .pieceUnableToMoveToLocation(description: "Piece at \(fromLocation) cannot move to \(toLocation)"))
         }
-        
-        // Move the piece
-        
-        print("Piece can be moved")
-        
+
+        return (true, nil)
     }
-    
+
 }
 
