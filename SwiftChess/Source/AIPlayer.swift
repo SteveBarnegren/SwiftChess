7da86e81fdcdb8bc8d9c16f63829dce9db142638
Steve Barnegren <steve.barnegren@gmail.com>
2016-12-29 13:48:41 +0000

    Add AI ability to promote pawns

--- SwiftChess/Source/AIPlayer.swift
+++ SwiftChess/Source/AIPlayer.swift
@@ -52,8 +52,18 @@ open class AIPlayer : Player {
                     continue
                 }
                 
+                // Make move
                 var resultBoard = board
                 resultBoard.movePiece(fromLocation: sourceLocation, toLocation: targetLocation)
+                
+                // Promote pawns
+                let pawnsToPromoteLocations = resultBoard.getLocationsOfPromotablePawns(color: color)
+                assert(pawnsToPromoteLocations.count < 2, "There should only ever be one pawn to promote at any time")
+                if pawnsToPromoteLocations.count > 0 {
+                    resultBoard = promotePawnsOnBoard(resultBoard)
+                }
+                
+                // Rate
                 let rating = ratingForBoard(resultBoard)
                 let move = Move(sourceLocation: sourceLocation, targetLocation: targetLocation, rating: rating)
                 possibleMoves.append(move)
@@ -84,7 +94,19 @@ open class AIPlayer : Player {
         print("HIGHEST MOVE RATING: \(highestRating)")
         
         // Make move
-        let operations = game.board.movePiece(fromLocation: highestRatedMove.sourceLocation, toLocation: highestRatedMove.targetLocation)
+        var operations = game.board.movePiece(fromLocation: highestRatedMove.sourceLocation, toLocation: highestRatedMove.targetLocation)
+        
+        // Promote pawns
+        let pawnsToPromoteLocations = game.board.getLocationsOfPromotablePawns(color: color)
+        assert(pawnsToPromoteLocations.count < 2, "There should only ever be one pawn to promote at any time")
+        if pawnsToPromoteLocations.count > 0 {
+            game.board = promotePawnsOnBoard(game.board)
+            
+            let location = pawnsToPromoteLocations.first!
+            let transformOperation = BoardOperation(type: .transformPiece, piece: game.board.getPiece(at: location)!, location: location)
+            operations.append(transformOperation)
+        }
+        
         self.game.playerDidMakeMove(player: self, boardOperations: operations)
     }
     
@@ -97,6 +119,53 @@ open class AIPlayer : Player {
         }
         
         return rating
+    }
+    
+ 
+    func promotePawnsOnBoard(_ board: Board) -> Board {
+        
+        let pawnsToPromoteLocations = board.getLocationsOfPromotablePawns(color: color)
+
+        guard pawnsToPromoteLocations.count > 0 else {
+            return board
+        }
+        
+        assert(pawnsToPromoteLocations.count < 2, "There should only ever be one pawn to promote at any time")
+        
+        let location = pawnsToPromoteLocations.first!
+        
+        guard let piece = board.getPiece(at: location) else {
+            return board
+        }
+        
+        // Get the ratings
+        var bestType = Piece.PieceType.possiblePawnPromotionResultingTypes().first
+        var bestRating = -Double.greatestFiniteMagnitude
+        var promotedBoard: Board!
+        
+        
+        for pieceType in Piece.PieceType.possiblePawnPromotionResultingTypes() {
+            
+            var newBoard = board
+            
+            guard let piece = newBoard.getPiece(at: location) else {
+                return board
+            }
+            
+            let newPiece = newBoard.getPiece(at: location)?.byChangingType(newType: pieceType)
+            newBoard.squares[location.index].piece = newPiece
+            
+            let rating = ratingForBoard(newBoard)
+            
+            if rating > bestRating {
+                bestRating = rating
+                bestType = pieceType
+                promotedBoard = newBoard
+            }
+            
+        }
+        
+        return promotedBoard
     }
     
     
