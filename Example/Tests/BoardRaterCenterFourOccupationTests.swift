2a3c883793f00315898b1b71ea62d9b102bd862c
Steve Barnegren <steve.barnegren@gmail.com>
2017-01-03 19:40:48 +0000

    Added BoardRaterCenterFourOccupation

---
+++ Example/Tests/BoardRaterCenterFourOccupationTests.swift
@@ -0,0 +1,123 @@
+//
+//  BoardRaterCenterFourOccupationTests.swift
+//  Example
+//
+//  Created by Steve Barnegren on 03/01/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftChess
+
+class BoardRaterCenterFourOccupationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func defaultBoardRater() -> BoardRaterCenterFourOccupation {
+        
+        let configuration = AIConfiguration()
+        let boardRater = BoardRaterCenterFourOccupation(configuration: configuration)
+        return boardRater
+    }
+    
+    func testNorthEastCenterSquareResultsInHigherRating() {
+        
+        let board = ASCIIBoard(pieces:  "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - P - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" )
+        
+        let rating = defaultBoardRater().ratingfor(board: board.board, color: .white)
+        XCTAssertGreaterThan(rating, 0)
+    }
+    
+    func testSouthEastCenterSquareResultsInHigherRating() {
+       
+        let board = ASCIIBoard(pieces:  "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - P - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" )
+        
+        let rating = defaultBoardRater().ratingfor(board: board.board, color: .white)
+        XCTAssertGreaterThan(rating, 0)
+    }
+    
+    func testSouthWestCenterSquareResultsInHigherRating() {
+        
+        let board = ASCIIBoard(pieces:  "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - P - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" )
+        
+        let rating = defaultBoardRater().ratingfor(board: board.board, color: .white)
+        XCTAssertGreaterThan(rating, 0)
+    }
+    
+    func testNorthWestCenterSquareResultsInHigherRating() {
+        
+        let board = ASCIIBoard(pieces:  "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - P - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" )
+        
+        let rating = defaultBoardRater().ratingfor(board: board.board, color: .white)
+        XCTAssertGreaterThan(rating, 0)
+    }
+    
+    func testNonCenterSquaresResultInZeroRating() {
+        
+        let board = ASCIIBoard(pieces:  "P P P P P P P P" +
+                                        "P P P P P P P P" +
+                                        "P P P P P P P P" +
+                                        "P P P - - P P P" +
+                                        "P P P - - P P P" +
+                                        "P P P P P P P P" +
+                                        "P P P P P P P P" +
+                                        "P P P P P P P P" )
+        
+        let rating = defaultBoardRater().ratingfor(board: board.board, color: .white)
+        XCTAssertEqualWithAccuracy(rating, 0, accuracy: 0.01)
+    }
+    
+    func testOpponentOccupationResultsInNegativeRating() {
+        
+        let board = ASCIIBoard(pieces:  "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - p p - - -" +
+                                        "- - - p p - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" +
+                                        "- - - - - - - -" )
+        
+        let rating = defaultBoardRater().ratingfor(board: board.board, color: .white)
+        XCTAssertLessThan(rating, 0)
+    }
+    
+   
+}
---
+++ SwiftChess/Source/BoardRaterCenterFourOccupation.swift
@@ -0,0 +1,39 @@
+//
+//  BoardRaterCenterFourOccupation.swift
+//  SwiftChess
+//
+//  Created by Steve Barnegren on 03/01/2017.
+//  Copyright © 2017 Steve Barnegren. All rights reserved.
+//
+
+import Foundation
+
+class BoardRaterCenterFourOccupation: BoardRater {
+    
+    override func ratingfor(board: Board, color: Color) -> Double {
+        
+        let value = Double(1);
+        var rating = Double(0)
+        
+        BoardLocation(x: 3, y: 3)
+        let locations = [
+            BoardLocation(x: 4, y: 4), // NE
+            BoardLocation(x: 4, y: 3), // SE
+            BoardLocation(x: 3, y: 3), // SW
+            BoardLocation(x: 3, y: 4), // NW
+                         ]
+        
+        for location in locations {
+            
+            guard let piece = board.getPiece(at: location) else {
+                continue
+            }
+            
+            rating += piece.color == color ? value : -value
+        }
+        
+        return rating
+    }
+    
+    
+}
