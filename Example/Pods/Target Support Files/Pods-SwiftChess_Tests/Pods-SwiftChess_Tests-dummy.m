beed7797b467eef7e4cd9f00bc3942d70a5f136f
Steve Barnegren <steve.barnegren@gmail.com>
2017-10-19 08:36:53 +0100

    Remove unused files from old project setup

--- Example/Pods/Target Support Files/Pods-SwiftChess_Tests/Pods-SwiftChess_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftChess_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftChess_Tests
-@end
--- Example/Pods/Target Support Files/SwiftChess/SwiftChess-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftChess : NSObject
-@end
-@implementation PodsDummy_SwiftChess
-@end
